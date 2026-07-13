e94245f9773107cfd219519531416468f74f1db7
zionmoron456 <zionmoron456@gmail.com>
2026-07-13 14:00:39 +0700

    Update cache version from v1 to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'report-activity-v1';
+const CACHE = 'report-activity-v2';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
